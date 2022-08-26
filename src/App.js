55068ea944ba21d70f8b2bb9b4341dab9c08afb6
Supa-Thobile2 <thobilenontenja@gmail.com>
2022-08-26 03:11:41 +0200

    updated project and fixed error

--- src/App.js
+++ src/App.js
@@ -1,3 +1,5 @@
+import { useState } from "react";
+
 import NewEmployee from "./components/NewEmployee";
 
 
@@ -6,14 +8,39 @@ import NewEmployee from "./components/NewEmployee";
 
 function App(){
 
+  const [employees, setEmployees] = useState([
+    {name: 'james', email: 'james21@gmail.com'},
+    {name: 'james', email: 'james21@gmail.com'},
+    {name: 'james', email: 'james21@gmail.com'}
+
+  ]);
+
+  const removeEmployee = index =>{
+    setEmployees([
+      ...employees.slice(0, index),
+      ...employees.slice(index +1, employees.length)
+    ])
+  }
+
+  const updateEmployee = (index, updatedEmployee, handleClose)=>{
+    setEmployees([
+      ...employees.slice(0, index),
+      updatedEmployee,
+      ...employees.slice(index +1, employees.length)
+
+    ])
+    handleClose()
+  }
+
 
   const submitNewEmployee = (newEmployee) =>{
+    console.log(newEmployee)
     
   }
   return(
     <div>
       
-      <NewEmployee/>
+      <NewEmployee submitNewEmployee={submitNewEmployee}/>
 
     </div>
   )
--- src/components/NewEmployee.js
+++ src/components/NewEmployee.js
@@ -1,17 +1,43 @@
-function NewEmployee(){
+import { useState } from "react";
+
+
+
+const NewEmployee = props =>{
+
+    const [employee, setEmployee] = useState({
+        email:'fff',
+        name:'eee'
+    });
+
+    const add = event =>{
+        event.preventDefault();
+        props.submitEmployee(employee)
+    };
+
+    
+    const handleChange = (event) =>{
+        event.preventDefault();
+        setEmployee({
+            ...employee,
+            [event.target.name]:event.target.value
+        })
+    }
+
     return(
         <div style = {{border: '1px solid #dedede', borderRadius: '20px', width: '80%', margin: '20px'}}>
             <label style = {{textAlign : 'center'}}>
                 Add New Employee
             </label>
 
-            <form>
+            <form onSubmit={add}>
 
                 <label for = "Name"/>
                 <input
                     type = "text"
                     name = "name"
                     id = "name"
+                    value ={employee.name}
+                    onChange={handleChange}
                 
                 />
 
@@ -20,6 +46,8 @@ function NewEmployee(){
                     type = "email"
                     name = "email"
                     id = "email"
+                    value ={employee.email}
+                    onChange={handleChange}
                 
                 />
 
